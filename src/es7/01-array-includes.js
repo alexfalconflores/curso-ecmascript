32fb645a4bd170313edd23d8b3177bd7aaece34f
Alex Stefano Falcon Flores <alexfalconflores@hotmail.com>
2022-08-29 22:30:03 -0500

    [ADD] ES7

---
+++ src/es7/01-array-includes.js
@@ -0,0 +1,6 @@
+let numbers = [1, 2, 3, 4, 5];
+console.log(numbers.includes(4)); // true
+
+const names = ['John', 'Paul', 'George', 'Ringo'];
+console.log(names.includes('John')); // true
+console.log(names.includes('john')); // false
